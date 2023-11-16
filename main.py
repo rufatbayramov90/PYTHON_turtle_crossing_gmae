7693d58eb319129c85bfec081ee1bd2166ddd64f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-11-16 16:28:05 +0400

    hit the car

--- main.py
+++ main.py
@@ -21,3 +21,8 @@ while game_is_on:
 
     car_manager.create_cars()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
